8e6bb4490f64527eb1c917d23f0fbea1a99180c4
satoshi watanabe <satosu3@gmail.com>
2020-11-08 09:29:01 +0000

    add sample.php

--- sample.php
+++ sample.php
@@ -1,26 +1,14 @@
-<?php
-    $result = '無し';
-    if (array_key_exists('act', $_POST)) {
-        $result = omikuji();
-    }
-    
-    function omikuji() {
-        $fortune = ['大吉','吉','中吉','小吉','末吉','凶'];
-        return $fortune[random_int(0, count($fortune) -1)];
-    }
-?>
-
-<!DOCTYPE htmel>
-<html lang='ja'>
+<!DOCTYPE html>
+<html lang="ja">
     <head>
-        <meta charset='UTF-8'>
+        <meta charset="UTF-8">
         <title>おみくじ</title>
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, 'UTF-8'); ?></p>
-        <form action='sample.php' method='POST'>
-            <button type='submit' name='act' value='draw'>おみくじをひく！</button>
+        <p>おみくじの結果：無し</p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
     </body>
 </html>
